819ed6d2a6103bb190e026068be8a9ead57d448d
DobbyzzUp <76682818+DobbyzzUp@users.noreply.github.com>
2021-03-07 11:59:58 -0300

    Add files via upload

---
+++ src/menuvideo.js
@@ -0,0 +1,27 @@
+const menuvideo = (prefix) => { 
+	return `➤🌌𝙈𝙀𝙉𝙐 𝘿𝙀 𝙑𝙄𝘿𝙀𝙊𝙎/𝙀𝘿𝙄𝙏𝙎🌌
+🌨🌨🌨🌨🌨🌨🌨🌨🌨🌨🌨🌨
+➤𝙀𝙙𝙞𝙩𝙨☔
+
+➦${prefix}𝘅𝘅𝘅1
+➦${prefix}𝘅𝘅𝘅2
+➦${prefix}𝘀𝗮𝘆1
+➦${prefix}𝘀𝗮𝘆2
+➦${prefix}𝘀𝗮𝘆3
+➦${prefix}𝘀𝗮𝘆4
+➦${prefix}𝗽𝗶𝘂
+━━━━━━━━━━━━━━━━━━🌨
+➤𝙑𝙞𝙙𝙚𝙤𝙨☔
+
+➦${prefix}
+➦${prefix}
+➦${prefix}
+➦${prefix}
+➦${prefix}
+➦${prefix}
+
+𝗠𝗮𝗶𝘀 𝗲𝗺 𝗯𝗿𝗲𝘃𝗲...
+🌨️🌨️🌨️🌨️🌨️🌨️🌨️🌨️🌨️🌨️🌨️🌨️
+𝘽𝙮 𝘿𝙤𝙗𝙗𝙮☔`
+}
+exports.menuvideo = menuvideo
